4bc7e328b4b3d8873fcf3e1d4fa49a23aa57622e
AkillesU <a.rechardt@gmail.com>
2023-04-17 16:39:07 +0100

    Added YAML config

--- build1.py
+++ build1.py
@@ -1,6 +1,7 @@
 import sklearn as skl
 import sksfa
 
+import yaml
 import random
 import re
 import os
@@ -10,11 +11,12 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-fps = 30
-length = 24
-video_path = "$HOME/projects/spatial-model/video1.mp4"
 
-#Defining the parameters for a frame
+#Loading Config file
+with open("config.yaml") as f:
+  config = yaml.safe_load(f)
+
+print(config)
 def crop_center_square(frame):
   y, x = frame.shape[0:2]
   min_dim = min(y, x)
@@ -23,7 +25,7 @@ def crop_center_square(frame):
   return frame[start_y:start_y+min_dim,start_x:start_x+min_dim]
 
 #Defining function to load video as array
-def load_video(path, max_frames=fps*length, resize=(320,40)):
+def load_video(path, max_frames= config['fps']*config['length'], resize=(320,40)):
   cap = cv2.VideoCapture(path)
   frames = []
   try:
@@ -43,27 +45,17 @@ def load_video(path, max_frames=fps*length, resize=(320,40)):
   return np.array(frames) / 255.0
 
 #loading video as array from "video_path"
-video = load_video(video_path)
+video = load_video(config['video_path'])
 
 #checking array shape [n_frames, width, height, RGB]
 print(video.shape)
-#plotting first frame
-#plt.imshow(video[0], interpolation="nearest")
-#plt.show()
 
-input_shape = [320,40,3]
-noise = 0.05
-final_degree = 2
-layerconfig = [(10,10,5,5,441,2),(7,3,4,4,30,2), (15,2,1,1,1,2)]
-components = 32
-batch_size = 32
-
-sfa_layers = sksfa.HSFA(n_components=components,
-                        input_shape = input_shape,
-                        internal_batch_size=batch_size,
-                        noise_std = noise,
-                        final_degree= final_degree,
-                        layer_configurations=layerconfig)
+sfa_layers = sksfa.HSFA(n_components=config['components'],
+                        input_shape = config['input_shape'],
+                        internal_batch_size=config['batch_size'],
+                        noise_std = config['noise'],
+                        final_degree= config['final_degree'],
+                        layer_configurations=config['layerconfig'])
 
 
 sfa_layers.fit(video)
